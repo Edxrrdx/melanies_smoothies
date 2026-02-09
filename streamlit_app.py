2335094ceeb28ca2e2c8e4ba6890766ae80ab14b
Edxrrdx <141583891+Edxrrdx@users.noreply.github.com>
2026-02-09 16:09:08 -0600

    Fix typo in use_container_width parameter

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if ingredients_list:
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     #st.text(smoothiefroot_response.json())
-    sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+    sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
